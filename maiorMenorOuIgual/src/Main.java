9cdb500da5e6610f284aad8dd2fe2728e7ad0251
Norberto Diman <norbertodimann@outlook.com.br>
2026-02-12 11:42:28 -0300

    added maiorMenorOuIgual files

---
+++ maiorMenorOuIgual/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int numberOne = 30;
+        int numberTwo = 30;
+
+        if (numberOne == numberTwo) {
+            System.out.println(numberOne + " = " + numberTwo);
+        } else if (numberOne > numberTwo) {
+            System.out.println(numberOne + " > " + numberTwo);
+        } else {
+            System.out.println(numberOne + " < " + numberTwo);
+        }
+    }
+}
