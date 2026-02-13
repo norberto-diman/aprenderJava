5f0c8b0403c9364c005d102051a3693c50157539
Norberto Diman <norbertodimann@outlook.com.br>
2026-02-13 11:03:14 -0300

    removed comments at lacoRepeticao files

--- lacoRepeticao/src/Main.java
+++ lacoRepeticao/src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-//        LacoRepeticao lr = new LacoRepeticao();
-//        lr.executaForNormal();
-//        lr.executaForEach();
-
         Matematica math = new Matematica();
-//        math.calcularTabuada();
-//        math.verificarParImpar();
         math.aplicarAumento();
     }
 }
