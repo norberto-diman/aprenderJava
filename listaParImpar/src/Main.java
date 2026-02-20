44eff5c66a0a7013d7b51d38518fdb267293c6bc
Norberto Diman <norbertodimann@outlook.com.br>
2026-02-20 19:37:04 -0300

    fix some bugs

--- listaParImpar/src/Main.java
+++ listaParImpar/src/Main.java
@@ -4,19 +4,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner valorUser = new Scanner(System.in);
+        ArrayList<Integer> listaNumero = new ArrayList<>();
         ArrayList<Integer> listaDeNumerosPares = new ArrayList<Integer>();
         ArrayList<Integer> listaDeNumerosImpares = new ArrayList<Integer>();
 
         for (int i = 0; i < 10; i++) {
             System.out.println("Digite um numero: ");
             Integer numero = valorUser.nextInt();
+            listaNumero.add(numero);
 
-            if (numero % 2 == 0) {
-                listaDeNumerosPares.add(numero);
+
+        }
+
+        for (Integer j : listaNumero) {
+            if (j % 2 == 0) {
+                listaDeNumerosPares.add(j);
             } else {
-                listaDeNumerosImpares.add(numero);
+                listaDeNumerosImpares.add(j);
             }
-
         }
 
         System.out.println("Pares: " + listaDeNumerosPares);
