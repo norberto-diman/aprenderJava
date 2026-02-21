73e72ff871961f65d1c0ac5303d87e193c8d88bf
Norberto Diman <norbertodimann@outlook.com.br>
2026-02-21 18:03:15 -0300

    add new files

--- map/src/ComparaString.java
+++ map/src/ComparaString.java
@@ -34,17 +34,24 @@ public class ComparaString {
         if (compare1.compareTo(compare2) == 0) {
             System.out.println(compare1 + " e " + compare2 + " são iguais!");
         }
-        if (compare1.compareTo(compare2) > 0){
-            System.out.println("");
+        if (compare1.compareTo(compare2) > 0) {
+            System.out.println(compare1 + " é " + "maior que " + compare2);
+        }
+        if (compare1.compareTo(compare2) < 0) {
+            System.out.println(compare2 + " é " + "maior que " + compare1);
         }
 
         String compareIgCase1 = "NorberTo Diman";
         String compareIgCase2 = "Norberto";
 
-        if (compareIgCase1.compareToIgnoreCase(compareIgCase2)) {
-            System.out.println("Verdadeiro");
-        } else {
-            System.out.println("Falso");
+        if (compareIgCase1.compareToIgnoreCase(compareIgCase2) == 0) {
+            System.out.println(compareIgCase1 + " e " + compareIgCase2 + " são iguais!");
+        }
+        if (compareIgCase1.compareToIgnoreCase(compareIgCase2) > 0) {
+            System.out.println(compareIgCase1 + " é " + "maior que " + compareIgCase2);
+        }
+        if (compareIgCase1.compareToIgnoreCase(compareIgCase2) < 0) {
+            System.out.println(compareIgCase2 + " é " + "maior que " + compareIgCase1);
         }
 
     }
